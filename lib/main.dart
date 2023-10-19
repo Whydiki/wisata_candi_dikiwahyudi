6ef2840df4043812a797274c8df754cc2076e98b
Diki Wahyudi <whydiki2308@gmail.com>
2023-10-19 11:25:53 +0700

    wisata candi

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wisata_candi/data/candi_data.dart';
 import 'package:wisata_candi/screens/detail_screen.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: DetailScreen(),
+      home: DetailScreen(candi: candiList[0]),
     );
   }
 }
--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,9 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:wisata_candi/models/candi.dart';
 class DetailScreen extends StatelessWidget {
-  const DetailScreen({super.key});
+  Candi candi;
+  DetailScreen({super.key, required this.candi});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        children: [
+          //Detail header
+          Stack(
+            children: [
+              //Image Utama
+              Image.asset(candi.imageAsset),
+
+            ],
+          ),
+          //Detail info
+          //Detail Galeri
+        ],
+      ),
+    );
   }
 }
